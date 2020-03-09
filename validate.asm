51df2e1e9a58fced4a99eb9161bc025891b0a128
Deji Adegbite <d2alphame@yahoo.com>
2020-03-09 19:37:55 +0100

    Make validate return matching characters as a length-prefixed string also take address of the destination buffer as paramater

--- validate.asm
+++ validate.asm
@@ -8,39 +8,51 @@ Bits 64									; Assemble to 64 bits code
 ;
 ; In
 ;	rsi: The string to validate
-;	rdi: The validation string. The string against which to validate
+;	rdi: The validation string. The string (length prefixed) against which to validate
+;	rdx: Where to put the consecutive matching characters (length prefixed string)
 ;
 ; Out
 ;	al: The first invalid character encountered
-;	rcx: Number of consecutive valid characters
+;	rdi: Pointer to string of consecutive matching characters
 ;	rsi: Points to the invalid character
 ;	cf: Clear
 ;
 ; Note that rax, rbx, rcx, rdx, rsi, rdi, and rbp will be in an unknown state by the
 ; end of this function. While r8-r15 along with the stack (rsp) will be
-; preserved
+; preserved. This function always clears the carry flag
 
 validate:
 	push r8							; Preserve the r8 register on the stack
 	xor rcx, rcx					; Clear the rcx register
+	mov rbp, rdx					; Save the destination buffer in rbp
 	xor rdx, rdx					; Clear the rdx register
 	mov rcx, dword [rdi]			; Load the length of the validation string into rcx
 	mov r8, rcx						; We do this because the scasb instruction will affect rcx
 	add rdi, 4						; Point rdi at the first actual character of the validation string
 	mov rbx, rdi					; Because scasb affects rdi
+	push rbp						; Save this as it will be need later
+	add rbp, 4						; Leave space for the length of the valid characters
 
 	.start:
 		lodsb						; Load the first character of the string to validate from rsi to al. Autoincrement rsi
 		repnz scasb					; Check for character in the validation string
 		jnz .done					; If the zero flag is still cleared then the character is not in the validation string
 		inc rdx						; If we get to this point then the character is valid so increment rdx
+		mov [rbp], al				; Write the valid character to memory
+		inc rbp						; Increment rbp for next write
 		mov rcx, r8					; Reload the length of the validation string (we're about to loop)
 		mov rdi, rbx				; Also reload the pointer to the first character of the validation string
 		jmp .start					; Return continue with the next character in the string to validate
 
 	.done:
+		pop rbp						; Get the address of the destination buffer which we had saved earlier
+		mov [rbp], edx				; Write out the 4-byte length of the string of matching characters
+		mov rdi, rbp				; Point rdi at the resulting string of characters
 		pop r8						; Restore the r8 register
 		dec rsi						; Point rsi back at the invalid character ('lodsb' incremented rsi)
-		mov rcx, rdx				; Get the count in rcx
 		clc							; Clear the carry flag
 		ret							; Return
+
+
+; Same as 'validate' above but does the opposite. 
+invalidate:
